38e382632dd189e7bca5667e32ad0b98c5d8c308
jakhongir001 <jakhongirabdukhamidov@gmail.com>
2021-03-26 10:34:53 +0500

    Chaining Challenge

--- Udemy JS Challenges/script.js
+++ Udemy JS Challenges/script.js
@@ -287,12 +287,37 @@ Test data:
 GOOD LUCK 
 */
 
-const calcAverageHumanAge = function (agesArray) {
-  const averageInHumanAge = agesArray
+// const calcAverageHumanAge = function (agesArray) {
+//   const averageInHumanAge = agesArray
+//     .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
+//     .filter(dog => dog > 18)
+//     .reduce((acc, dAge, i, arr) => acc + dAge / arr.length, 0);
+//   console.log(averageInHumanAge);
+// };
+// calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+// calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+/*
+Coding Challenge #3
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+GOOD LUCK �
+*/
+// In the last one I had done most of it this way already
+
+const calcAverageHumanAge = agesArray =>
+  agesArray
     .map(age => (age <= 2 ? age * 2 : 16 + age * 4))
     .filter(dog => dog > 18)
     .reduce((acc, dAge, i, arr) => acc + dAge / arr.length, 0);
-  console.log(averageInHumanAge);
-};
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
